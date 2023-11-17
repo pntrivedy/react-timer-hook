c2178734f9dab73da7918632ef51f796f327e562
pntrivedy <me@preraktrivedi.com>
2023-11-17 11:36:06 +0530

    Fix useStopwatch autoStart on reset even if set false.

--- src/useStopwatch.js
+++ src/useStopwatch.js
@@ -3,14 +3,21 @@ import { Time } from './utils';
 import { useInterval } from './hooks';
 
 export default function useStopwatch({ autoStart, offsetTimestamp } = {}) {
-  const [passedSeconds, setPassedSeconds] = useState(Time.getSecondsFromExpiry(offsetTimestamp, true) || 0);
+  const [passedSeconds, setPassedSeconds] = useState(
+    Time.getSecondsFromExpiry(offsetTimestamp, true) || 0,
+  );
   const [prevTime, setPrevTime] = useState(new Date());
-  const [seconds, setSeconds] = useState(passedSeconds + Time.getSecondsFromPrevTime(prevTime || 0, true));
+  const [seconds, setSeconds] = useState(
+    passedSeconds + Time.getSecondsFromPrevTime(prevTime || 0, true),
+  );
   const [isRunning, setIsRunning] = useState(autoStart);
 
-  useInterval(() => {
-    setSeconds(passedSeconds + Time.getSecondsFromPrevTime(prevTime, true));
-  }, isRunning ? 1000 : null);
+  useInterval(
+    () => {
+      setSeconds(passedSeconds + Time.getSecondsFromPrevTime(prevTime, true));
+    },
+    isRunning ? 1000 : null,
+  );
 
   const start = useCallback(() => {
     const newPrevTime = new Date();
@@ -24,16 +31,22 @@ export default function useStopwatch({ autoStart, offsetTimestamp } = {}) {
     setIsRunning(false);
   }, [seconds]);
 
-  const reset = useCallback((offset = 0, newAutoStart = true) => {
+  const reset = useCallback((offset = 0, newAutoStart = autoStart) => {
     const newPassedSeconds = Time.getSecondsFromExpiry(offset, true) || 0;
     const newPrevTime = new Date();
     setPrevTime(newPrevTime);
     setPassedSeconds(newPassedSeconds);
     setIsRunning(newAutoStart);
-    setSeconds(newPassedSeconds + Time.getSecondsFromPrevTime(newPrevTime, true));
+    setSeconds(
+      newPassedSeconds + Time.getSecondsFromPrevTime(newPrevTime, true),
+    );
   }, []);
 
   return {
-    ...Time.getTimeFromSeconds(seconds), start, pause, reset, isRunning,
+    ...Time.getTimeFromSeconds(seconds),
+    start,
+    pause,
+    reset,
+    isRunning,
   };
 }
